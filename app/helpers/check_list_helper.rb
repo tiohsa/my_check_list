d9f20337a0c893f3f73a9080e303f6462d3829b0
glorydays <glorydays11@gmail.com>
2024-03-25 08:03:53 +0000

    add check_list module

--- app/helpers/check_list_helper.rb
+++ app/helpers/check_list_helper.rb
@@ -1,2 +1,18 @@
 module CheckListHelper
+  def update_issue_done_ratio(issue)
+    items = CheckItem.where(issue: issue)
+    issue.done_ratio = calc_progress(items, issue)
+    if issue.done_ratio == 100
+      issue.status_id = 5
+    end
+    issue.save
+  end
+
+  def calc_progress(items, issue)
+    items_completed = items.filter{ |item| item.is_done }
+    total = issue.children.length + items.length
+    subtasks = issue.children
+    subtasks_completed = subtasks.filter{|subtask| subtask.done_ratio == 100 || subtask.status_id == 5}
+    (subtasks_completed.length.to_f + items_completed.length.to_f) / total.to_f * 100.0
+  end
 end
--- lib/my_check_list/hooks.rb
+++ lib/my_check_list/hooks.rb
@@ -1,5 +1,6 @@
 module MyCheckList
   class Hooks < Redmine::Hook::ViewListener
+    include CheckListHelper
 
     def view_issues_show_description_bottom(context = {})
       checklist = []
@@ -51,23 +52,6 @@ module MyCheckList
       edit_items.each do |edit_item|
         edit_item.save
       end
-    end
-
-    def update_issue_done_ratio(issue)
-      items = CheckItem.where(issue: issue)
-      issue.done_ratio = calc_progress(items, issue)
-      if issue.done_ratio == 100
-        issue.status_id = 5
-      end
-      issue.save
-    end
-
-    def calc_progress(items, issue)
-      items_completed = items.filter{ |item| item.is_done }
-      total = issue.children.length + items.length
-      subtasks = issue.children
-      subtasks_completed = subtasks.filter{|subtask| subtask.done_ratio == 100 || subtask.status_id == 5}
-      (subtasks_completed.length.to_f + items_completed.length.to_f) / total.to_f * 100.0
     end
 
     def items_params(context={})
