fbea53a7c16303b3830c2a849770cc8bd0a11d8f
glorydays <glorydays11@gmail.com>
2024-03-25 23:56:17 +0900

    fix migration file

--- db/migrate/20240323175044_create_check_items.rb
+++ db/migrate/20240323175044_create_check_items.rb
@@ -1,7 +1,7 @@
 class CreateCheckItems < ActiveRecord::Migration[6.1]
   def change
     create_table :check_items do |t|
-      t.integer :issue_id
+      t.references :issue, foreign_key: true, type: :integer
       t.integer :order_num
       t.string :text
       t.boolean :is_done, default: false
--- db/migrate/20240324142930_change_check_items.rb
+++
@@ -1,6 +0,0 @@
-class ChangeCheckItems  < ActiveRecord::Migration[6.1]
-  def change
-    # remove_column :check_items, :issue_id
-    add_reference :check_items, :issue, foreign_key: true
-  end
-end
