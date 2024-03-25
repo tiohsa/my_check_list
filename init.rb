281936744cbeeb34bc844144f5be449ce0fe0bf1
glorydays <glorydays11@gmail.com>
2024-03-25 23:09:55 +0900

    change redmine plugin info

--- init.rb
+++ init.rb
@@ -1,12 +1,11 @@
 require 'redmine'
 require_dependency File.expand_path('../lib/my_check_list/hooks', __FILE__)
-# require_dependency 'my_check_list/hooks'
 
 Redmine::Plugin.register :my_check_list do
-  name 'My Check List plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'redmine check list plugin'
+  # author 'tiohsa'
+  description 'This is a check list plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  # url 'https://github.com/tiohsa/my_check_list'
+  # author_url 'https://github.com/tiohsa/my_check_list'
 end
