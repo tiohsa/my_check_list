6eefd2d949e3799923075dfda6bdfd2721f6042b
glorydays <glorydays11@gmail.com>
2024-03-27 00:18:04 +0900

    fix delete checklist

--- app/controllers/check_list_controller.rb
+++ app/controllers/check_list_controller.rb
@@ -7,7 +7,7 @@ class CheckListController < ApplicationController
     is_doen = params[:is_done].downcase == "true"
     check_item = CheckItem.find_by(issue: id, order_num: order_num)
     check_item.is_done = is_doen
-    check_item.save
+    check_item.save!
     update_issue_done_ratio(Issue.find(id))
     render json: { check_item: check_item }
   end
--- lib/my_check_list/hooks.rb
+++ lib/my_check_list/hooks.rb
@@ -28,13 +28,17 @@ module MyCheckList
       items = items_params(context)
       issue = context[:issue]
       save_checklist(items, issue)
-      # update_issue_done_ratio(issue)
     end
 
     def controller_issues_edit_after_save(context={})
       items = items_params(context)
       issue = context[:issue]
-      CheckItem.destroy_by(issue: issue)
+
+      delete_item_ids = get_delete_ids(items, issue)
+      delete_item_ids.each do |delete_item_id|
+        CheckItem.destroy(delete_item_id)
+      end
+
       save_checklist(items, issue)
       update_issue_done_ratio(issue)
       update_parent_progress(issue)
@@ -42,23 +46,40 @@ module MyCheckList
 
     private
 
+    def get_delete_ids(items, issue)
+      registered_checklist = CheckItem.where(issue: issue)
+      registered_check_item_ids = registered_checklist.map do |registered_check_item|
+        registered_check_item.id
+      end
+      item_ids = items.map do |item|
+        item[:id].to_i if item[:id] != -1
+      end.compact
+      delete_ids = registered_check_item_ids - item_ids
+      delete_ids
+    end
+
     def save_checklist(items, issue)
       edit_items = []
       items.each_with_index do |item, index|
-        check_item = CheckItem.new(item)
+        check_item = CheckItem.find_by(id: item[:id].to_i)
+        if !check_item.present?
+          check_item = CheckItem.new(item)
+        end
         check_item.issue = issue
+        check_item.text = item[:text]
+        check_item.is_done = item[:is_done]
         check_item.order_num = index
         edit_items << check_item
       end
       edit_items.each do |edit_item|
-        edit_item.save
+        edit_item.save!
       end
     end
 
     def items_params(context={})
       if context[:params][:items].present?
         context[:params].require(:items).map do |item|
-          item.permit(:text, :is_done) if !item[:text].blank?
+          item.permit(:text, :is_done, :id) if !item[:text].blank?
         end.compact
       else
         {}
